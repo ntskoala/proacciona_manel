689c8391a519cc61faa0b7cf0fb2bd760d9fa154
jorge.dominguez <jorged@ntskoala.com>
2016-09-21 08:51:41 +0200

    Empresas Service y listado empresas en plan cutrillo

--- app/empresas/empresas.component.ts
+++ app/empresas/empresas.component.ts
@@ -1,10 +1,29 @@
 import {Component} from '@angular/core';
+import {EmpresasService} from './empresas.service';
 
 @Component({
     selector: 'empresas',
     templateUrl: 'public/assets/templates/empresas.component.html',
-    styleUrls: ['public/assets/css/empresas.component.css']
+    styleUrls: ['public/assets/css/empresas.component.css'],
+    providers: [EmpresasService]
 })
 export class EmpresasComponent {
+public empresas:any;
+private response:any;
+    constructor(private empresasService: EmpresasService) {
+        this.empresasService.getEmpresas().subscribe(
+            data => {
+            this.response = JSON.stringify(data);
+            console.log (this.response);
+            this.empresas = data;
+            }
+        );
+    };
+
+
+seleccionaempresa(empresa){
+    alert('Empresa seleccionada id: ' + empresa.idempresa + ' nombre: ' + empresa.nombreempresa);
+}
+
 
 }
