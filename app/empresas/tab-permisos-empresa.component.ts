237b06428b70efa75c488e864a3ee21ab307d1d3
Manel Romero <manel@manelromero.com>
2016-10-12 10:58:08 +0200

    Añadir tablas de permisos en tabulador

--- app/empresas/tab-permisos-empresa.component.ts
+++ app/empresas/tab-permisos-empresa.component.ts
@@ -7,6 +7,8 @@ import {URLS} from '../config';
 import {Usuario} from '../objetos/usuario';
 import {Control} from '../objetos/control';
 import {Checklist} from '../objetos/checklist';
+import {PermissionUserControl} from '../objetos/permissionusercontrol';
+import {PermissionUserChecklist} from '../objetos/permissionuserchecklist';
 
 @Component({
     selector: 'tab-permisos-empresa',
@@ -16,25 +18,33 @@ import {Checklist} from '../objetos/checklist';
 export class TabPermisosEmpresaComponent {
 
     private subscription: Subscription;
+    public usuario: Usuario = new Usuario(0, 'Seleccionar usuario', '', '', '', 0)
     public usuarios: Usuario[] = [];
     public controles: Control[] = [];
     public checklists: Checklist[] = [];
-    public seleccionada: number;
+    public permissionusercontrols: PermissionUserControl[] = [];
+    public permissionuserchecklists: PermissionUserChecklist[] = [];
+    public empresaSeleccionada: number = 0;
+    public usuarioSeleccionado: number = 0;
 
     constructor(private servidor: Servidor, private empresasService: EmpresasService) {
         this.subscription = this.empresasService.empresaSeleccionada.subscribe(
-            seleccionada => this.seleccionarUsuario()
-        );
+            seleccionada => {
+                this.usuarioSeleccionado = 0;
+                this.empresaSeleccionada = seleccionada.id;
+                this.seleccionarPermisos();
+        });
     }
 
-    seleccionarUsuario() {
-        this.seleccionada = this.empresasService.seleccionada;
+    seleccionarPermisos() {
+        this.empresaSeleccionada = this.empresasService.seleccionada;
         let token = sessionStorage.getItem('token');
-        let parametros = '?idempresa=' + this.seleccionada + '&token=' + token;
+        let parametros = '?idempresa=' + this.empresaSeleccionada + '&token=' + token;
         // conseguir usuarios
         this.servidor.llamadaServidor('GET', URLS.USUARIOS, parametros).subscribe(
             response => {
                 this.usuarios = [];
+                this.usuarios.push(this.usuario);
                 if (response.success && response.data) {
                     for (let i = 0; i < response.data.length; i++) {
                         this.usuarios.push(new Usuario(
@@ -86,5 +96,39 @@ export class TabPermisosEmpresaComponent {
         });
     }
 
+    seleccionarUsuario(idUsuario: number) {
+        this.usuarioSeleccionado = idUsuario;
+        let token = sessionStorage.getItem('token');
+        let parametros = '?idusuario=' + idUsuario + '&token=' + token;
+        // conseguir permissionusercontrol
+        this.servidor.llamadaServidor('GET', URLS.PERMISSION_USER_CONTROL, parametros).subscribe(
+            response => {
+                this.permissionusercontrols = [];
+                if (response.success && response.data) {
+                    for (let i = 0; i < response.data.length; i++) {
+                        this.permissionusercontrols.push(new PermissionUserControl(
+                            response.data[i].id,
+                            response.data[i].idcontrol,
+                            response.data[i].idusuario
+                        ));
+                    }
+                }
+        });
+        // conseguir permissionuserchecklist
+        this.servidor.llamadaServidor('GET', URLS.PERMISSION_USER_CHECKLIST, parametros).subscribe(
+            response => {
+                this.permissionuserchecklists = [];
+                if (response.success && response.data) {
+                    for (let i = 0; i < response.data.length; i++) {
+                        this.permissionuserchecklists.push(new PermissionUserChecklist(
+                            response.data[i].id,
+                            response.data[i].idchecklist,
+                            response.data[i].idusuario
+                        ));
+                    }
+                }
+        });
+
+    }
 
 }
